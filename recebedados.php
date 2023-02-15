b85cf512f68065d72758b36daff9d796fd68f6a8
JhonatanLucena <jhonatan.jhon9@hotmail.com>
2023-02-15 19:18:04 -0400

    Primeiro commit

---
+++ recebedados.php
@@ -0,0 +1,10 @@
+<?php 
+    $nome = $_POST['fname'];
+    $Sexo = $_POST['Sexo'];
+    echo $nome;
+    echo "<br>";
+    echo $Sexo;
+    echo "<br>";
+    echo "<button><a href='index.php'>voltar inicio</a></button>";
+    
+    ?>
